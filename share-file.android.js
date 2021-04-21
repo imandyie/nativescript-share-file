3c3250bce1e6e1aab93564bbc0f1114f6534702b
Andy <voxxxlll@gmail.com>
2021-04-21 17:00:00 +0100

    Fix for Android 11

--- share-file.android.js
+++ share-file.android.js
@@ -23,6 +23,7 @@ var ShareFile = (function () {
                 application.android.foregroundActivity.startActivity(android.content.Intent.createChooser(intent, args.intentTitle ? args.intentTitle : 'Open file:'));
             }
             catch (e) {
+              console.log(e)
                 console.log('ShareFile: Android intent failed');
             }
         }
@@ -38,7 +39,7 @@ var ShareFile = (function () {
     };
     ShareFile.prototype._getUriForPath = function (path, fileName, ctx) {
         if (path.indexOf("file:///") === 0) {
-            return this._getUriForAbsolutePath(path);
+            return this._getUriForAbsolutePath(path, ctx);
         }
         else if (path.indexOf("file://") === 0) {
             return this._getUriForAssetPath(path, fileName, ctx);
@@ -55,9 +56,8 @@ var ShareFile = (function () {
             }
         }
     };
-    ShareFile.prototype._getUriForAbsolutePath = function (path) {
+    ShareFile.prototype._getUriForAbsolutePath = function (path, ctx) {
         var absPath = path.replace("file://", "");
-        var file = new java.io.File(absPath);
         if (!file.exists()) {
             console.log("File not found: " + file.getAbsolutePath());
             return null;
@@ -68,17 +68,14 @@ var ShareFile = (function () {
     };
     ShareFile.prototype._getUriForAssetPath = function (path, fileName, ctx) {
         path = path.replace("file://", "/");
-        if (!fs.File.exists(path)) {
-            console.log("File does not exist: " + path);
-            return null;
-        }
-        var localFile = fs.File.fromPath(path);
-        var localFileContents = localFile.readSync(function (e) { console.log(e); });
-        var cacheFileName = this._writeBytesToFile(ctx, fileName, localFileContents);
-        if (cacheFileName.indexOf("file://") === -1) {
-            cacheFileName = "file://" + cacheFileName;
-        }
-        return android.net.Uri.parse(cacheFileName);
+        var file = new java.io.File(path);
+
+        return androidx.core.content.FileProvider.getUriForFile(
+          application.android.foregroundActivity ||
+          application.android.startActivity,
+          application.android.packageName + ".fileprovider",
+          file
+      );
     };
     ShareFile.prototype._getUriForBase64Content = function (path, fileName, ctx) {
         var resData = path.substring(path.indexOf("://") + 3);
